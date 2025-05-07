58d68d4aefe7a47c71f69fc47dd0c309121d6d09
c8villanueva <caitlin.villanueva@gmail.com>
2025-05-07 23:44:45 +0800

    connected mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const path = require('path')
-const { logger } = require('./middleware/logger')
+const { logger, logEvents } = require('./middleware/logger')
 const errorHandler = require('./middleware/errorHandler')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 3500
+
+console.log(process.env.NODE_ENV)
+
+connectDB()
 
 // keep before everything else
 app.use(logger)
@@ -68,4 +75,13 @@ app.all(/.*/, (req, res) => {
 app.use(errorHandler)
 
 //always at the end of server.js
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB')
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+// when you try to do smth without an internet connection for example
+mongoose.connection.on('error', err => {
+  console.log(err)
+  logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
