ca3289120bfde64f490728d9d60bf00dae9e4687
c8villanueva <caitlin.villanueva@gmail.com>
2025-05-07 23:13:34 +0800

    added some middleware + loggers + error handing

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,45 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
 const PORT = process.env.PORT || 3500
+
+// keep before everything else
+app.use(logger)
+
+// Cross-Origin Resource Sharing  - for security
+// tbh the deployment issue might be coming from here so we'll see
+// placing everything in this file for now for easier troubleshooting later
+const allowedOrigins = [
+  'http://localhost:3000', //for development, where the frontend will be
+  //fake urls for now
+  'https://www.dandrepairshop.com',
+  'https://dandreparishop.com',
+]
+const corsOptions = {
+  origin: (origin, callback) => {
+    //only those in the array can access
+    //but also block testing apps like postman, so include || !origin
+    if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200 //the default is 204, some devices have a problem with this
+}
+app.use(cors(corsOptions))
+
+//for the ability to process json
+app.use(express.json())
+
+// 3rd party middleware, to parse cookies
+app.use(cookieParser())
 
 // to allow access to all files in /public
 app.use('/', express.static(path.join(__dirname, '/public')))
@@ -14,8 +52,7 @@ app.use('/', require('./routes/root'))
 
 
 
-// errors 404
-// new regex syntax
+// shows 404 error page when non-existent routes are requested
 app.all(/.*/, (req, res) => {
   res.status(404)
   if (req.accepts('html')) {
@@ -28,7 +65,7 @@ app.all(/.*/, (req, res) => {
 })
 
 
-
+app.use(errorHandler)
 
 //always at the end of server.js
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
