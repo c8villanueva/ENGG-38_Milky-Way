54221e809eb4d29d36269f4f878b42beb40b65c9
c8villanueva <caitlin.villanueva@gmail.com>
2025-05-08 15:11:52 +0800

    added login page (no css yet)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
-import Landing from './components/Landing';
+import Landing from './components/Landing'
+import Login from './features/auth/Login'
 
 function App() {
   return (
@@ -7,11 +8,11 @@ function App() {
     //   <h1>Milky Way</h1>
     // </div>
     <Routes>
-      <Route path="/" element={<Landing />}>
+      <Route path="/" element={<Landing />}/>
+      <Route path="login" element={<Login />} />
       
         {/* <Route index element={<Landing />} /> ^ this will be the default */}
-      
-      </Route>
+    
 
     </Routes>
   );
