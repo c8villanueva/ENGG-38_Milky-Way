82f57ed60a186156413428b717ab95d926751bcf
Lorraine Malubag <malubaglorraine@gmail.com>
2025-05-11 23:41:09 +0800

    insane styling hahahaha

--- frontend/src/features/workspace/Workspace.js
+++ frontend/src/features/workspace/Workspace.js
@@ -4,7 +4,7 @@ import { useNavigate, Link, useLocation } from 'react-router-dom';
 
 import { useSendLogoutMutation } from '../auth/authApiSlice';
 
-// import './workspace.css';
+// import 'frontend/src/styles/workspace.css';
 
 // const WORKSPACE_REGEX = /^\/workspace(\/)?$/
 
@@ -13,6 +13,143 @@ const defaultStyles = {
   top: '50px',
   left: '50px',
 };
+
+const navbarStyles = {
+  navbar: {
+    background: '#81689d',
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '1.2rem',
+    position: 'sticky',
+    top: 0,
+    zIndex: 999,
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: '80px',
+    width: '100%',
+    margin: '0 auto',
+    padding: '0 50px',
+    alignItems: 'center',
+    zIndex: 1,
+  },
+  logo: {
+    backgroundColor: '#ffd0ec',
+    backgroundSize: '100%',
+    WebkitBackgroundClip: 'text',
+    MozBackgroundClip: 'text',
+    WebkitTextFillColor: 'transparent',
+    MozTextFillColor: 'transparent',
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+    textDecoration: 'none',
+    fontSize: '2rem',
+  },
+  menu: {
+    display: 'flex',
+    alignItems: 'center',
+    listStyle: 'none',
+    textAlign: 'center',
+    gap: '10px',
+    position: 'relative',
+  },
+  menuItem: {
+    position: 'relative',
+    // height: '80px',
+  },
+  link: {
+    color: '#ffd0ec',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textDecoration: 'none',
+    padding: '0 1rem',
+    height: '100%',
+    transition: 'color 0.3s ease',
+  },
+  // dropdownContainer: {
+  //     position: 'relative',
+  //   },
+  //   dropdownButton: {
+  //     display: 'flex',
+  //     justifyContent: 'center',
+  //     alignItems: 'center',
+  //     padding: '10px',
+  //     border: 'none',
+  //     background: '#ffd0ec',
+  //     cursor: 'pointer',
+  //   },
+  //   dropdownMenu: {
+  //         display: 'block',
+  //         position: 'absolute',
+  //         top: '100%',
+  //         right: 0,
+  //         backgroundColor: '#ffd0ec',
+  //         padding: '10px 0',
+  //         zIndex: 1000,
+  //       }
+  //   dropdownLink: {
+  //     padding: '8px 16px',
+  //     cursor: 'pointer',
+  //     textDecoration: 'none',
+  //     color: '#1f2544',
+  //   },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textDecoration: 'none',
+    padding: '10px 10px',
+    margin: '0 0.15rem',
+    width: '100%',
+    border: 'none',
+    outline: 'none',
+    borderRadius: '20px',
+    background: '#ffd0ec',
+    color: '#1f2544',
+    cursor: 'pointer',
+  },
+}
+
+const elmenuStyles = {
+  deleteEl: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'transparent',
+    color: 'black',
+    border: 'none',
+    width: '20px',
+    height: '20px',
+    cursor: 'pointer',
+    zIndex: 2,
+  },
+  pickcolor: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    top: 0,
+    right: '20px',
+    zIndex: 2,
+    border: 'none',
+    width: '20px',
+    height: '20px',
+    cursor: 'pointer',
+  },
+  resizer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    height: '20px',
+    width: '20px',
+    cursor: 'pointer',
+    backgroundColor: 'transparent',
+    border: 'none',
+  },
+}
 
 export default function Workspace() {
 
@@ -168,19 +305,19 @@ export default function Workspace() {
         )}
         {el.type === 'timer' && <Timer id={el.id} element={el} updateElement={updateElement} />}
         {el.type === 'list' && <TodoList id={el.id} content={el.content} updateElement={updateElement} />}
-        
+
         <div style={{ display: 'flex', alignItems: 'center', marginTop: 5 }}>
           <input type="color"
             value={el.type === 'text' ? el.textColor : el.color}
-            style={{ width: 20, height: 20, padding: 0, border: 'none', marginRight: 5 }}
+            style={elmenuStyles.pickcolor}
             onClick={e => e.stopPropagation()}
             onChange={e => {
               if (el.type === 'text') updateElement(el.id, { textColor: e.target.value });
               else updateElement(el.id, { style: { ...el.style, backgroundColor: e.target.value }, color: e.target.value });
             }}
           />
-          <button onClick={() => deleteElement(el.id)}>x</button>
-          <button className="resize-handle" onMouseDown={startResize(document.getElementById(el.id), el.id)}>+</button>
+          <button onClick={() => deleteElement(el.id)} style={elmenuStyles.deleteEl}>x</button>
+          <button className="resize-handle" onMouseDown={startResize(document.getElementById(el.id), el.id)} style={elmenuStyles.resizer}>+</button>
         </div>
       </div>
     );
@@ -381,39 +518,32 @@ export default function Workspace() {
     <section>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inclusive+Sans&display=swap"></link>
 
-      <nav className="navbar">
-        <div className="navbar__container">
+      <nav className="navbar" style={navbarStyles.navbar}>
+        <div className="navbar__container" style={navbarStyles.container}>
           {/* <a href="/" id="navbar__logo">
             <i className="fa-solid fa-rocket">&nbsp;</i> Milky Way
           </a> */}
-          <li id="navbar__logo">
+          <li id="navbar__logo" style={navbarStyles.logo}>
             <Link to="/">Milky Way</Link>
           </li>
-          <ul className="navbar__menu">
-            <li><button onClick={changeLayout}>[]</button></li>
-            <li><button onClick={() => addElement('text')}>text</button></li>
-            {/* <li className="navbar__dropdown"><button>shapes</button>
-              <div className="navbar__dropdown-content">
-                <button onClick={() => addElement('circle')}>circle</button> */}
-            <li><button onClick={() => addElement('square')}>square</button></li>
-            {/* </div> */}
-            {/* </li> */}
-            {/* <li className="navbar__dropdown"><button>others</button>
+          <ul className="navbar__menu" style={navbarStyles.menu}>
+
+            <li><button onClick={changeLayout} style={navbarStyles.button}>[]</button></li>
+            
+            <li><button onClick={() => addElement('text')} style={navbarStyles.button}>text</button></li>
+            <li><button onClick={() => addElement('square')} style={navbarStyles.button}>square</button></li>
+            <li><button onClick={() => addElement('timer')} style={navbarStyles.button}>timer</button></li>
+            <li><button onClick={() => addElement('list')} style={navbarStyles.button}>list</button></li>
+            {/* <li className="navbar__dropdown"><button style={navbarStyles.button}>⚙️</button>
               <div className="navbar__dropdown-content"> */}
-            <li><button onClick={() => addElement('timer')}>timer</button></li>
-            <li><button onClick={() => addElement('list')}>list</button></li>
-            {/* </div>
-            </li> */}
-            <li className="navbar__dropdown"><button>⚙️</button>
-              <div className="navbar__dropdown-content">
-                <button onClick={clearWorkspace}>clear</button>
-                <button onClick={saveWorkspace}>save</button>
-                <button onClick={loadWorkspace}>load</button>
+                <li><button onClick={clearWorkspace} style={navbarStyles.button}>clear</button></li>
+                <li><button onClick={saveWorkspace} style={navbarStyles.button}>save</button></li>
+                {/* <button onClick={loadWorkspace} style={navbarStyles.button}>load</button> */}
                 {/* <button onClick={sendLogout}>logout</button> */}
-                <button onClick={handleLogout}>logout</button>
+                <li><button onClick={handleLogout} style={navbarStyles.button}>logout</button></li>
                 {/* <button onClick={async () => await sendLogout()}>logout</button> */}
-              </div>
-            </li>
+              {/* </div>
+            </li> */}
           </ul>
         </div>
       </nav>
@@ -423,8 +553,9 @@ export default function Workspace() {
         height: '80vh',
         overflow: 'hidden',
         backgroundImage:
-          isGrid ? 'radial-gradient(circle,#aaa 1px,transparent 1px)' :
-            'linear-gradient(to right,#ccc 1px,transparent),(to bottom,#ccc 1px,transparent)',
+          isGrid 
+            ? 'radial-gradient(circle,#aaa 1px,transparent 1px)' 
+            : 'linear-gradient(to right,#ccc 1px,transparent),(to bottom,#ccc 1px,transparent)',
         backgroundSize: '20px 20px'
       }}>
         {elements.map(renderElement)}
